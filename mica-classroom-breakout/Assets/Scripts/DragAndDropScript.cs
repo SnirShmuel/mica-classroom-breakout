9d96c703608502246c28299cfd09bb39d4931cb8
SnirShmuel <37809417+SnirShmuel@users.noreply.github.com>
2021-07-14 00:00:59 +0300

    check win
    check win

--- mica-classroom-breakout/Assets/Scripts/DragAndDropScript.cs
+++ mica-classroom-breakout/Assets/Scripts/DragAndDropScript.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,6 +8,7 @@ public class DragAndDropScript : MonoBehaviour
 {
     public GameObject SelectedPiece;
     int OrderInLayer = 1;
+    private bool isFinish = false;
 
 
     // Start is called before the first frame update
@@ -52,5 +54,29 @@ public class DragAndDropScript : MonoBehaviour
             Vector3 MousePoint = Camera.main.ScreenToWorldPoint(Input.mousePosition);
             SelectedPiece.transform.position = new Vector3(MousePoint.x, MousePoint.y, 0);
         }
+
+        checkWin();
+
+        
+        // Check win
+        if (isFinish)
+        {
+            Application.Quit();
+        }
+    }
+
+    private void checkWin()
+    {
+        bool isInPlace = true;
+        for (int i = 0; i < 36; i++)
+        {
+            if (!GameObject.Find("Piece (" + i + ")").GetComponent<PuzzleGameScript>().isInRightPosition)
+            {
+                isInPlace = false;
+            }
+        }
+
+        if (isInPlace)
+            isFinish = true;
     }
 }
