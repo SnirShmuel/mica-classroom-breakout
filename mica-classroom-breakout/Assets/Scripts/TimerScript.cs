98abe79f0a2ca4b5842cab2358c35a6eefc4e6fe
SnirShmuel <37809417+SnirShmuel@users.noreply.github.com>
2021-07-17 20:46:42 +0300

    Game timer
    add game timer

---
+++ mica-classroom-breakout/Assets/Scripts/TimerScript.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using TMPro;
+
+public class TimerScript : MonoBehaviour
+{
+    float currentTime;
+    public int startMinutes;
+    public TextMeshProUGUI currentTimeText;
+    bool isTimerOver = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        currentTime = startMinutes * 60;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isTimerOver)
+        {
+            currentTime -= Time.deltaTime;
+            TimeSpan time = TimeSpan.FromSeconds(currentTime);
+            currentTimeText.text = time.Hours.ToString() + ":" + time.Minutes.ToString() + ":" + time.Seconds.ToString();
+        }
+        if (currentTime <= 0)
+        {
+            isTimerOver = true;
+        }
+    }
+}
