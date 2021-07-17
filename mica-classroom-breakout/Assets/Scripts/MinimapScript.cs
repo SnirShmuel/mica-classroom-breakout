9fd0659ade12b9e9574d3d07075a0681be250ff7
SnirShmuel <37809417+SnirShmuel@users.noreply.github.com>
2021-07-17 18:54:17 +0300

    fix minimap camera
    make minimap camera follow between floors

--- mica-classroom-breakout/Assets/Scripts/MinimapScript.cs
+++ mica-classroom-breakout/Assets/Scripts/MinimapScript.cs
@@ -9,7 +9,7 @@ public class MinimapScript : MonoBehaviour
     private void LateUpdate()
     {
         Vector3 newPosition = player.position;
-        newPosition.y = transform.position.y;
+        newPosition.y = player.position.y + 3;
         transform.position = newPosition;
 
         transform.rotation = Quaternion.Euler(90f, player.eulerAngles.y, 0f);
