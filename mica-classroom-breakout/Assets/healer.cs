6372f03e759477e1d2bf1934d8663739e11da5bb
Idan <killingfactoryidan@gmail.com>
2021-07-24 20:07:35 +0300

    recoil  & healer

---
+++ mica-classroom-breakout/Assets/healer.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class healer : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        HealthManager hm = other.gameObject.GetComponent<HealthManager>();
+        if (hm) {
+            hm.Heal();
+        }    
+    }
+}
